973b4652186c4f2292cad8de68a2a163dbeea570
kmk87 <minkyoungk24@gmail.com>
2024-08-15 19:33:00 +0900

    [스프링프로젝트] log4j2 설정

--- src/main/java/com/mk/spring/HomeController.java
+++ src/main/java/com/mk/spring/HomeController.java
@@ -1,5 +1,7 @@
 package com.mk.spring;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,9 +9,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
+	private static final Logger LOGGER = LogManager.getLogger(HomeController.class);
+
+	
 	// 클라이언트가 요청한 서비스 주소와 메소드를 연결
 	@RequestMapping(value= {"","/"}, method=RequestMethod.GET)
 	public String home() {
+		LOGGER.info("게시판 프로그램 시작");
 		return "home";
 	}
 	
